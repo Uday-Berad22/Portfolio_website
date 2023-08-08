936d706dc61429ffd401ec0ae2d9339428e3894d
uday berad <beradug22@gmail.com>
2023-08-09 00:19:37 +0530

    Pused all files

---
+++ index.js
@@ -0,0 +1,12 @@
+
+console.log("UDAY");
+
+  let xyz=document.getElementById("contact");
+  xyz.addEventListener("click",()=>{
+      Swal.fire({
+          icon: 'success',
+          title: 'Phone Number : 8010106564',
+          text: 'Email: udayberad50@gmail.com',
+          footer: '<a href="https://www.linkedin.com/in/uday-berad-108661238/" target="_b">Know more About me on Linkedin </a>'
+        })
+  })
